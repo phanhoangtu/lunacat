b63fb57b1977b1a7993d4dd1f6c459ac10688f10
tuph <tuph@vng.com.vn>
2024-10-24 00:18:46 +0700

    fix issue first loading images

--- main.js
+++ main.js
@@ -4,18 +4,31 @@ const images = [
     'assets/image-03.svg',
     'assets/image-04.svg'
 ];
+
 let currentIndex = 0;
+
 const preloadImages = images.map(src => {
     const img = new Image();
     img.src = src;
     return img;
 });
+
 const backgroundContainer = document.querySelector('.background-container');
-let currentImageIndex = 0;
-function changeBackground() {
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    
-    backgroundContainer.style.backgroundImage = `url(${images[currentImageIndex]})`;
-}
+
 backgroundContainer.style.backgroundImage = `url(${images[0]})`;
-setInterval(changeBackground, 3500);
+
+let imagesLoaded = 0;
+
+preloadImages.forEach((img) => {
+    img.onload = () => {
+        imagesLoaded++;
+        if (imagesLoaded === preloadImages.length) {
+            setInterval(changeBackground, 3500);
+        }
+    };
+});
+
+function changeBackground() {
+    currentIndex = (currentIndex + 1) % images.length;
+    backgroundContainer.style.backgroundImage = `url(${images[currentIndex]})`;
+}
