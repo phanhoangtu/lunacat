29ec2b6f33d8a2018645c12d2266efff9e505535
tuph <tuph@vng.com.vn>
2024-10-29 23:22:26 +0700

    Add fading layer

--- main.js
+++ main.js
@@ -11,22 +11,21 @@ const images = [
 
 let currentIndex = 0;
 const backgroundContainer = document.querySelector('.background-container');
+const nextBackground = document.querySelector('.next-background');
 
 backgroundContainer.style.backgroundImage = `url(${images[currentIndex]})`;
 
-function loadNextImage(index) {
-    const img = new Image();
-    img.src = images[index];
-    img.onload = () => {
-        if (index === currentIndex) {
-            backgroundContainer.style.backgroundImage = `url(${img.src})`;
-        }
-    };
-}
-
 function changeBackground() {
     currentIndex = (currentIndex + 1) % images.length;
-    loadNextImage(currentIndex);
+    const nextImage = images[currentIndex];
+
+    nextBackground.style.backgroundImage = `url(${nextImage})`;
+    nextBackground.classList.add('fade-in');
+
+    nextBackground.addEventListener('transitionend', () => {
+        backgroundContainer.style.backgroundImage = `url(${nextImage})`;
+        nextBackground.classList.remove('fade-in');
+    }, { once: true });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
